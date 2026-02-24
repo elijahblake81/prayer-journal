27f9f6918f02968dce861b37bf1aa395a4d7c1d5
Elijahblake <elijahblake81@gmail.com>
2026-02-24 13:41:14 -0600

    Updated main.jsx again

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-
+import { AuthProvider } from './lib/AuthProvider'
 
 // Register the service worker for PWA (auto updates)
 import { registerSW } from 'virtual:pwa-register'
